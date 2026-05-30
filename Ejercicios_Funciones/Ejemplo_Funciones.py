f8d04cb71e794360db05ab8ed477eb50262eca33
dacordovam-eng <da.cordovam@duocuc.cl>
2026-05-30 11:24:25 -0400

    Ejemplo de funciones

---
+++ Ejercicios_Funciones/Ejemplo_Funciones.py
@@ -0,0 +1,31 @@
+#Funciones
+
+def sumarDosnumeros():
+    '''Esta funcion permite sumar dos numeros,
+    ingresados dentro de la funcion'''
+
+    num1= int(input("Ingrese numero 1: "))
+    num2 = int(input("Ingrese numero 2: "))
+
+    suma = num1 + num2
+
+    print(f"La suma de {num1} + {num2} es = {suma}")
+
+def sumar(a,b) :
+    '''Esta funcion permite sumar dos numeros
+    ingresados por parametros'''
+
+    suma = a + b
+    return suma
+
+#Linea ppal
+sumarDosnumeros()
+
+res = sumar(4,5)
+
+print(res)
+
+num1 = int(input("Numero 1: "))
+num2 = int(input("Numero 2: "))
+res = sumar(num1,num2)
+print("La suma es: ", res)
